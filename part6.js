a8c9a8ad5403bfa8ae3016da2e39421efbd96117
Aswinkl71 <99712645+aswinaswinkl@users.noreply.github.com>
2022-02-17 10:38:14 +0530

    Add files via upload

---
+++ part6.js
@@ -0,0 +1,90 @@
+/*function myA()
+{
+    let a =5;
+    let b =10;
+    let c =a+b;
+   return c;
+}
+console.log(myA());/*/
+
+/*function com()
+{
+    console.log ("completed");
+}
+function myA()
+{
+    let a =5;
+    com();
+    return a;
+
+}
+console.log(myA());/*/
+
+/*alert("aswin");/*/
+
+/*function myA(z)
+{
+    let a =5;
+    let b = z;
+    let c =a+b;
+    return c;
+}
+console.log(myA(10));/*/
+/*let myA = {
+    aname : "aswin",
+    bname : "kl71"
+};
+console.log(myA);/*/
+/*
+let myArray = ["aswin","kl71"]
+console.log(myArray[1]);/*/
+/*
+let a = "aswink";
+console.log(a.length);/*/
+/*
+let a ="aswin";
+console.log(a.indexOf('n'));/*/
+
+/*let a ="aswin";
+console.log(a.lastIndexOf('n'));/*/
+/*let a ="aswin";
+console.log(a.search("aas"));/*/
+/*let a ="aswin";
+console.log(a.includes("aswin"))/*/
+/*let a = "aswin";
+let b =a.slice(2 - 4);
+console.log(b);/*/
+
+/*let  a = "15";
+let b = 10;
+console.log ( parseInt(a));/*/
+
+/*let myArray = ["a","b","c"];
+
+myArray [0] = "z";
+console.log(myArray);/*/
+/*let myArray = ["a","b","c"];
+myArray.push('d');
+console.log(myArray);/*/
+
+/*let myArray = ["a","b","c"];
+myArray.unshift('d');
+console.log(myArray);/*/
+
+/*let myArray = ["a","b","c"];
+myArray.shift();
+console.log(myArray);/*/
+/*
+let myArray = ["a","b","c","d"];
+console.log(myArray.length);/*/
+/*/let myArray = ["a","b","c"];
+myArray.splice (2,0 ,"d","e");
+console.log(myArray);/*/
+
+
+
+
+
+
+
+
